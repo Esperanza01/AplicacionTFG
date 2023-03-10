1168f1bada0ba59c408d50e42e3efa2988ef987d
Esperanza <esperanzamadridr@gmail.com>
2023-03-10 18:43:32 +0100

    layouts del splash screen y login

---
+++ app/src/main/java/com/gestorproyectos_v01/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.gestorproyectos_v01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/gestorproyectos_v01/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.gestorproyectos_v01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
